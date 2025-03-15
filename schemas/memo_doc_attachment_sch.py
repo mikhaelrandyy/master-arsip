60f52d87e6ecfa7ea1ed242baa461a160c464338
ginanjar <ginanjar.sumantri@agungsedayu.com>
2025-03-15 08:18:25 +0700

    -

---
+++ schemas/memo_doc_attachment_sch.py
@@ -0,0 +1,14 @@
+from models.memo_doc_attachment_model import MemoDocAttachmentBase, MemoDocAttachmentFullBase
+
+class MemoDocAttachmentCreateSch(MemoDocAttachmentBase):
+    pass
+
+class MemoDocAttachmentSch(MemoDocAttachmentFullBase):
+    pass 
+
+class MemoDocAttachmentUpdateSch(MemoDocAttachmentBase):
+    pass
+
+class MemoDocAttachmentByIdSch(MemoDocAttachmentFullBase):
+    pass
+
