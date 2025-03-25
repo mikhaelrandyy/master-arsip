df9a841750b64d20e3289daa1ed5b4812d958fc0
Mikhael Randy <mikhael.briantono@agungsedayu.com>
2025-03-25 14:59:07 +0700

    fix [enum] pisah enum customer dev

--- common/enum.py
+++ common/enum.py
@@ -57,44 +57,6 @@ class CodeCounterEnum(str, Enum):
     DOC_TYPE_GROUP = "GJD"
     MEMO = "ASG"
 
-class CustomerDevEnum(str, Enum):
-    PERSON = "person"
-    ORGANIZATION = "organization"
-    PERSON_GROUP = "person group"
-
-class JenisIdentitasEnum(str, Enum):
-    KTP = "ktp"
-    NIB = "nib"
-    KIA = "kia"
-    PASPOR = "paspor"
-
-class ReligionEnum(str, Enum):
-    ISLAM = "islam"
-    KRISTEN = "kristen"
-    KATHOLIK ="katholik"
-    HINDU = "hindu"
-    BUDDHA = "buddha"
-    KONGHUCU = "khonghucu"
-    UNKNOWN = "-"
-
-class GenderEnum(str, Enum):
-    MALE = "laki - laki"
-    FEMALE = "perempuan"
-
-class MaritalStatusEnum(str, Enum):
-    BELUM_KAWIN = "belum kawin"
-    KAWIN = "kawin"
-    CERAI_HIDUP = "cerai hidup"
-    CERAI_MATI = "cerai mati"
-    UNKNOWN = "-"
-
-class AddressEnum(str, Enum):
-    HOME = "rumah"
-    OFFICE = "kantor"
-    COMPANY = "perusahaan"
-    WAREHOUSE = "gudang"
-    OTHER = "lainnya"
-    UNKNOWN = "-"
 
 class PhysicalDocTypeEnum(str, Enum):
     ASLI = "ASLI"
---
+++ common/enum_customer.py
@@ -0,0 +1,41 @@
+
+from enum import Enum
+
+class CustomerDevEnum(str, Enum):
+    PERSON = "person"
+    ORGANIZATION = "organization"
+    PERSON_GROUP = "person group"
+
+class JenisIdentitasEnum(str, Enum):
+    KTP = "ktp"
+    NIB = "nib"
+    KIA = "kia"
+    PASPOR = "paspor"
+
+class ReligionEnum(str, Enum):
+    ISLAM = "islam"
+    KRISTEN = "kristen"
+    KATHOLIK ="katholik"
+    HINDU = "hindu"
+    BUDDHA = "buddha"
+    KONGHUCU = "khonghucu"
+    UNKNOWN = "-"
+
+class GenderEnum(str, Enum):
+    MALE = "laki - laki"
+    FEMALE = "perempuan"
+
+class MaritalStatusEnum(str, Enum):
+    BELUM_KAWIN = "belum kawin"
+    KAWIN = "kawin"
+    CERAI_HIDUP = "cerai hidup"
+    CERAI_MATI = "cerai mati"
+    UNKNOWN = "-"
+
+class AddressEnum(str, Enum):
+    HOME = "rumah"
+    OFFICE = "kantor"
+    COMPANY = "perusahaan"
+    WAREHOUSE = "gudang"
+    OTHER = "lainnya"
+    UNKNOWN = "-"
